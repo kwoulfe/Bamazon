cb720b7df40e860010058f61aa04ac252eeb7c77
Kevin Woulfe <kevinpwoulfe@gmail.com>
2018-04-19 14:41:21 -0700

    Add start function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,3 +13,24 @@ connection.connect(function(err) {
   console.log('connected as id: ' + connection.threadId + '\n');
   start();
 });
+
+var start = function() {
+  var query = 'SELECT * FROM Products';
+  connection.query(query, function(err, res) {
+    for (i = 0; i < res.length; i++) {
+      console.log(
+        'Item ID: ' +
+          res[i].item_id +
+          ' || Product: ' +
+          res[i].product_name +
+          ' || Department: ' +
+          res[i].department_name +
+          ' || Price: ' +
+          res[i].price +
+          ' || Stock: ' +
+          res[i].stock_quantity
+      );
+    }
+    buy();
+  });
+};
